fee8521d4c031f26f6817e959353a2791f18ceb0
Sam Mahdad <smmahdad@Sams-MacBook-Pro.local>
2016-02-15 21:13:29 -0800

    Finished hw2 with my keyboard. It was quite fun to mess around and make my first app-ish thing.

--- CustomKeyboard/CalKeyboard/KeyboardViewController.swift
+++ CustomKeyboard/CalKeyboard/KeyboardViewController.swift
@@ -6,12 +6,46 @@
 //  Copyright © 2015 iOS Decal. All rights reserved.
 //
 
+//PLEASE NOTE: I know that the function names should be more
+//practical, but I wanted to have fun with the project and the function names.
+
 import UIKit
 
 class KeyboardViewController: UIInputViewController {
 
     @IBOutlet var nextKeyboardButton: UIButton!
     
+    @IBOutlet weak var swerveButton: UIButton!
+    
+    @IBOutlet weak var kanyeButton: UIButton!
+    
+    @IBOutlet weak var returnButton: UIButton!
+    
+    @IBOutlet weak var deleteButton: UIButton!
+    
+    @IBOutlet weak var fireButton: UIButton!
+    
+    @IBOutlet weak var fuegoButton: UIButton!
+    
+    @IBOutlet weak var sameButton: UIButton!
+    
+    @IBOutlet weak var freshmanButton: UIButton!
+    
+    @IBOutlet weak var spaceBar: UIButton!
+    
+    @IBOutlet weak var creamButton: UIButton!
+    
+    @IBOutlet weak var fuckingButton: UIButton!
+    
+    @IBOutlet weak var upeButton: UIButton!
+    
+    @IBOutlet weak var wapButton: UIButton!
+    
+    @IBOutlet weak var bernieButton: UIButton!
+    
+    @IBOutlet weak var yeeeButton: UIButton!
+    
+
     var keyboardView: UIView!
 
     override func updateViewConstraints() {
@@ -23,6 +57,7 @@ class KeyboardViewController: UIInputViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         loadInterface()
+        loadButtons()
     }
 
     override func didReceiveMemoryWarning() {
@@ -36,6 +71,101 @@ class KeyboardViewController: UIInputViewController {
 
     override func textDidChange(textInput: UITextInput?) {
         // The app has just changed the document's contents, the document context has been updated.
+    }
+    
+    
+    
+    func kanyee() {
+        (textDocumentProxy as UIKeyInput).insertText("You may be talented but you're not Kanye")
+    }
+    
+    func checkIfSwerve() {
+        (textDocumentProxy as UIKeyInput).insertText("swerve")
+    }
+    
+    func returnThatShit() {
+        (textDocumentProxy as UIKeyInput).insertText("\n")
+    }
+    
+    func shawtyOnFire() {
+        (textDocumentProxy as UIKeyInput).insertText("fire")
+    }
+    
+    func deleteOne() {
+        (textDocumentProxy as UITextDocumentProxy).deleteBackward()
+    }
+    
+    func shawtyOnFuego() {
+        (textDocumentProxy as UIKeyInput).insertText("fuego")
+    }
+    
+    func mightyYiwen() {
+        (textDocumentProxy as UIKeyInput).insertText("same")
+    }
+    
+    func freshmanFreshman() {
+        (textDocumentProxy as UIKeyInput).insertText("freshman")
+    }
+    
+    func inSpace() {
+        (textDocumentProxy as UIKeyInput).insertText(" ")
+    }
+    
+    func creamEverywhere() {
+        (textDocumentProxy as UIKeyInput).insertText("cream")
+    }
+    
+    func fuckingA() {
+        (textDocumentProxy as UIKeyInput).insertText("fuckin'")
+    }
+    
+    func premierFunction() {
+        (textDocumentProxy as UIKeyInput).insertText("Upsilon Pi Epsilon: The Premier CS Honors Society")
+    }
+    
+    func wapWap() {
+        (textDocumentProxy as UIKeyInput).insertText("Got a glock in my 'rari")
+    }
+    
+    func feelTheBern() {
+        (textDocumentProxy as UIKeyInput).insertText("#FeelTheBern")
+    }
+    
+    func kCarlYeee() {
+        (textDocumentProxy as UIKeyInput).insertText("yeee")
+    }
+    
+    func loadButtons() {
+        swerveButton.addTarget(self, action: "checkIfSwerve", forControlEvents: .TouchUpInside)
+        
+        kanyeButton.addTarget(self, action: "kanyee", forControlEvents: .TouchUpInside)
+        
+        returnButton.addTarget(self, action: "returnThatShit", forControlEvents: .TouchUpInside)
+        
+        deleteButton.addTarget(self, action: "deleteOne", forControlEvents: .TouchUpInside)
+        
+        fireButton.addTarget(self, action: "shawtyOnFire", forControlEvents: .TouchUpInside)
+        
+        fuegoButton.addTarget(self, action: "shawtyOnFuego", forControlEvents: .TouchUpInside)
+        
+        sameButton.addTarget(self, action: "mightyYiwen", forControlEvents: .TouchUpInside)
+        
+        freshmanButton.addTarget(self, action: "freshmanFreshman", forControlEvents: .TouchUpInside)
+        
+        spaceBar.addTarget(self, action: "inSpace", forControlEvents: .TouchUpInside)
+        
+        creamButton.addTarget(self, action: "creamEverywhere", forControlEvents: .TouchUpInside)
+        
+        fuckingButton.addTarget(self, action: "fuckingA", forControlEvents: .TouchUpInside)
+        
+        upeButton.addTarget(self, action: "premierFunction", forControlEvents: .TouchUpInside)
+        
+        wapButton.addTarget(self, action: "wapWap", forControlEvents: .TouchUpInside)
+        
+        bernieButton.addTarget(self, action: "feelTheBern", forControlEvents: .TouchUpInside)
+        
+        yeeeButton.addTarget(self, action: "kCarlYeee", forControlEvents: .TouchUpInside)
+
     }
 
     func loadInterface() {
